74037518dfd25362eec4f85e136c95a9bdf7624e
matheus.leonardo <matheusleotrab@gmail.com>
2022-11-27 15:19:04 -0300

    mandatory - update pipex header
    update the pipex header with the new function's prototypes, new macros and new struct

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 18:24:53 by mleonard          #+#    #+#             */
-/*   Updated: 2022/11/27 11:41:03 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/11/27 15:16:59 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+typedef struct s_pipex
+{
+	int		infile;
+	char	*cmd_1;
+	char	*cmd_path_1;
+	char	*cmd_2;
+	char	*cmd_path_2;
+	int		outfile;
+}				t_pipex;
+
 // I/O macros
 # define STDIN 0
 # define STDOUT 1
@@ -24,5 +34,21 @@
 // Macros related to error status
 # define ERR -1
 # define OK 0
+
+// Macros related to the pipe ends
+# define OUT_END 0
+# define IN_END 1
+
+// Validate user input functions
+int		validate_input(int argc);
+
+// Parser related functions
+t_pipex	*parse_input(char *argv[], t_pipex *pipex_data);
+
+// Error handlers
+int		error_func(char *func_name);
+
+// Main functions
+int		shutdown_pipex(t_pipex *pipex_data);
 
 #endif
