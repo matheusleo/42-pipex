adbbe636b186bb5a3bddbf54dcd9daa456bdcd61
matheus.leonardo <matheusleotrab@gmail.com>
2022-11-27 18:23:37 -0300

    andatory - improve parsing
    improve parsing by removing memory leaks and adding a new error handler

--- source/parser.c
+++ source/parser.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 12:25:26 by mleonard          #+#    #+#             */
-/*   Updated: 2022/11/27 17:32:46 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/11/27 18:19:14 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static int	get_fd(char *file_name)
 
 	fd = open(file_name, O_CREAT | O_RDWR | O_TRUNC);
 	if (fd == ERR)
-		error_func("open");
+		error_open(file_name);
 	return (fd);
 }
 
@@ -30,16 +30,18 @@ static char	*parse_program_path(char *cmd)
 	cmd_splitted = ft_split(cmd, ' ');
 	cmd_path = ft_strjoin("/bin/", cmd_splitted[0]);
 	free_vector(cmd_splitted);
+	if (access(cmd_path, X_OK) == ERR)
+		error_access(cmd);
 	return (cmd_path);
 }
 
 t_pipex	*parse_input(char *argv[], t_pipex *pipex_data)
 {
 	pipex_data->infile = get_fd(argv[1]);
+	pipex_data->outfile = get_fd(argv[4]);
 	pipex_data->cmd_1 = ft_split(argv[2], ' ');
 	pipex_data->cmd_path_1 = parse_program_path(argv[2]);
 	pipex_data->cmd_2 = ft_split(argv[3], ' ');
 	pipex_data->cmd_path_2 = parse_program_path(argv[3]);
-	pipex_data->outfile = get_fd(argv[4]);
 	return (pipex_data);
 }
