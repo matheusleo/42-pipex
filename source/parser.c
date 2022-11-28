35d12d3c6f76d08b240f97071835dabab93d7044
matheus.leonardo <matheusleotrab@gmail.com>
2022-11-28 02:13:18 -0300

    mandatory - fix parser
    fix parser to avoid delete the content from the infile before even reading it

--- source/parser.c
+++ source/parser.c
@@ -6,17 +6,17 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 12:25:26 by mleonard          #+#    #+#             */
-/*   Updated: 2022/11/27 18:27:52 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/11/28 02:11:45 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
 
-static int	get_fd(char *file_name, t_pipex *pipex_data)
+static int	get_fd(char *file_name, int flags, t_pipex *pipex_data)
 {
 	int	fd;
 
-	fd = open(file_name, O_CREAT | O_RDWR | O_TRUNC);
+	fd = open(file_name, O_RDWR | flags);
 	if (fd == ERR)
 		error_open(file_name, pipex_data);
 	return (fd);
@@ -37,8 +37,8 @@ static char	*parse_program_path(char *cmd, t_pipex *pipex_data)
 
 t_pipex	*parse_input(char *argv[], t_pipex *pipex_data)
 {
-	pipex_data->infile = get_fd(argv[1], pipex_data);
-	pipex_data->outfile = get_fd(argv[4], pipex_data);
+	pipex_data->infile = get_fd(argv[1], 0, pipex_data);
+	pipex_data->outfile = get_fd(argv[4], O_CREAT | O_TRUNC, pipex_data);
 	pipex_data->cmd_1 = ft_split(argv[2], ' ');
 	pipex_data->cmd_path_1 = parse_program_path(argv[2], pipex_data);
 	pipex_data->cmd_2 = ft_split(argv[3], ' ');
