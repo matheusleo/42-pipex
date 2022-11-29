81e0ef13081414377f4fa1a6609551f08afd486f
matheus.leonardo <matheusleotrab@gmail.com>
2022-11-29 00:52:04 -0300

    bonus - update the main function
    update the main function after the previous updates related to the header, parser and validator functions

--- source/main_bonus.c
+++ source/main_bonus.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 18:24:46 by mleonard          #+#    #+#             */
-/*   Updated: 2022/11/27 18:52:31 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/11/29 00:36:08 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pipex.h>
+#include <pipex_bonus.h>
 
 static t_pipex	init_pipex_struct(void)
 {
 	t_pipex	data;
 
 	data.infile = 0;
-	data.cmd_1 = NULL;
-	data.cmd_path_1 = NULL;
-	data.cmd_2 = NULL;
-	data.cmd_path_2 = NULL;
+	data.cmds = NULL;
+	data.cmds_len = 0;
 	data.outfile = 1;
 	data.status = OK;
 	data.pipe[0] = 0;
@@ -30,10 +28,8 @@ static t_pipex	init_pipex_struct(void)
 
 int	shutdown_pipex(t_pipex *pipex_data)
 {
-	free_vector(pipex_data->cmd_1);
-	free_vector(pipex_data->cmd_2);
-	free(pipex_data->cmd_path_1);
-	free(pipex_data->cmd_path_2);
+	if (pipex_data->cmds)
+		free(pipex_data->cmds);
 	close(pipex_data->infile);
 	close(pipex_data->outfile);
 	exit(pipex_data->status);
@@ -45,7 +41,7 @@ int	main(int argc, char *argv[])
 
 	validate_input(argc);
 	pipex_data = init_pipex_struct();
-	parse_input(argv, &pipex_data);
+	parse_input(argc, argv, &pipex_data);
 	pipex(&pipex_data);
 	pipex_data.status = OK;
 	shutdown_pipex(&pipex_data);
