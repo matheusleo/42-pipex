6da16f647a458764e46e3e4e2494b573905b3d43
matheus.leonardo <matheusleotrab@gmail.com>
2022-11-29 00:42:57 -0300

    bonus - update validate input function
    update the validator function to validate according to the bonus project

--- source/validate_bonus.c
+++ source/validate_bonus.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate.c                                         :+:      :+:    :+:   */
+/*   validate_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 12:11:36 by mleonard          #+#    #+#             */
-/*   Updated: 2022/11/27 12:29:01 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/11/29 00:36:08 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pipex.h>
+#include <pipex_bonus.h>
 
 int	validate_input(int argc)
 {
-	if (argc != 5)
+	if (argc < 5)
 	{
 		ft_printf("ERR: input does not attend the requirements\n");
-		ft_printf("Expected to be: <infile> '<cmd1>' '<cmd2>' <outfile>\n");
+		ft_printf("Expected to be: <infile> '<cmd1>' '<cmd2>'");
+		ft_printf("'<cmdN>' ... <outfile>\n");
 		exit (ERR);
 	}
 	return (OK);
