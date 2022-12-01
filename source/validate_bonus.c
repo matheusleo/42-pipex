8080561cdae8a3088a69d85e2b736ef3bacdc94f
matheus.leonardo <matheusleotrab@gmail.com>
2022-12-01 02:16:12 -0300

    bonus - support to here_doc
    add support to the here_doc when validating the user input

--- source/validate_bonus.c
+++ source/validate_bonus.c
@@ -6,19 +6,27 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 12:11:36 by mleonard          #+#    #+#             */
-/*   Updated: 2022/11/29 23:51:53 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/12/01 02:14:54 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex_bonus.h>
 
-int	validate_input(int argc)
+int	validate_input(int argc, char *argv[])
 {
-	if (argc < 5)
+	if (ft_strncmp(argv[1], "here_doc", 8) == 0 && argc != 6)
 	{
 		ft_printf("ERR: input does not attend the requirements\n");
-		ft_printf("Expected to be: <infile> '<cmd1>' '<cmd2>' ");
-		ft_printf("... '<cmdN>' <outfile>\n");
+		ft_printf("Expected to be:\n	");
+		ft_printf("./pipex here_doc LIMITER '<cmd1>' '<cmd2>' <outfile>\n");
+		exit (ERR);
+	}
+	else if (argc < 5)
+	{
+		ft_printf("ERR: input does not attend the requirements\n");
+		ft_printf("Expected to be: \n	");
+		ft_printf("./pipex <infile> '<cmd1>' '<cmd2>' ... '<cmdN>' <outfile>\n");
+		ft_printf("	OR ./pipex here_doc LIMITER '<cmd1>' '<cmd2>' <outfile>\n");
 		exit (ERR);
 	}
 	return (OK);
